42c12facff78aaf103a1f0c3d69a9fc76051295a
Alok Menghrajani <alok@squareup.com>
2015-04-10 12:38:40 -0700

    Adds GroupJooqDao which has the same API as GroupDAO.

---
+++ server/src/main/java/keywhiz/service/daos/GroupJooqMapper.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2015 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package keywhiz.service.daos;
+
+import keywhiz.api.model.Group;
+import keywhiz.jooq.tables.records.GroupsRecord;
+import org.jooq.Record;
+import org.jooq.RecordMapper;
+
+class GroupJooqMapper implements RecordMapper<Record, Group> {
+  public Group map(Record record) {
+    GroupsRecord r = (GroupsRecord) record;
+    return new Group(r.getId(), r.getName(), r.getDescription(), r.getCreatedat(), r.getCreatedby(),
+        r.getUpdatedat(), r.getUpdatedby());
+  }
+}
